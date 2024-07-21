21cfc3ffcdb1e4a61607ea7adfdaf8fb0abd1789
NikhileshS <nikhileshs2004@gmail.com>
2024-07-22 01:31:58 +0530

    Made changes to the UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+
+
+const dialog = document.querySelector('dialog');
+
+const btn = document.querySelector('.add-btn');
+btn.addEventListener('click',()=>{
+    dialog.showModal();
+})
+
+exit.addEventListener('click',() =>{
+    dialog.close();
+    form.reset();
+})
