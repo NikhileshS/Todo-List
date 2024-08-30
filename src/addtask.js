645fdcfb4acc940eeed4302f31244a76666989ac
NikhileshS <nikhileshs2004@gmail.com>
2024-08-31 01:08:39 +0530

    Added Priority selection and Task completion

--- src/addtask.js
+++ src/addtask.js
@@ -1,42 +1,63 @@
 
-
 const addtaskfun = function (){
     const title = document.getElementById('title').value;
     const task = document.getElementById('task').value;
-    const prior = document.getElementById('Priority').value;
+    const prior = document.getElementById('Priority').checked;
 
     if(title === '' | task === ''){
         alert("Fill the valid details");
         const form = document.querySelector('.taskform').reset();
     }
     else{
-        container(title, task);
+        container(title, task,prior);
     }
 }
 
-const container = function (title,task){
+const container = function (title,task,prior){
     const cont = document.createElement('div');
-    const tasktitle = document.createElement('div');
+    const tasktitle = document.createElement('btn');
     const taskdesc = document.createElement('div');
     const removebtn = document.createElement('btn');
     const content = document.querySelector('.content');
+    const complete = document.createElement('btn');
+    const btncon = document.createElement('div');
+    const editbtn = document.createElement('btn');
+
 
     cont.classList.add("taskcont");
     tasktitle.classList.add('tasktitle');
     taskdesc.classList.add('taskdesc');
+    complete.classList.add('completed');
+    editbtn.classList.add('editbtn')
+    btncon.classList.add('btncon');
 
-    tasktitle.textContent = `task : ${title}`;
-    taskdesc.textContent = task;
+    tasktitle.textContent = `Task : ${title}`;
+    taskdesc.textContent = 'Description';
     removebtn.innerHTML = 'Remove'
     removebtn.classList.add('removebtn')
+    complete.innerHTML = 'Completed';
+    editbtn.innerHTML = 'Edit';
+
+    if(prior){
+        cont.style.border = 'solid';
+        cont.style.borderColor = 'red';
+    }
+
+    complete.addEventListener('click',()=>{
+        cont.style.border = 'solid';
+        cont.style.borderColor = 'green';
+    })
 
     removebtn.addEventListener('click',()=>{
         content.removeChild(cont);
     })
 
     cont.appendChild(tasktitle);
-    cont.appendChild(taskdesc);
-    cont.appendChild(removebtn);
+    btncon.appendChild(taskdesc);
+    btncon.appendChild(editbtn);
+    btncon.appendChild(removebtn);
+    btncon.appendChild(complete);
+    cont.appendChild(btncon)
     content.appendChild(cont);
 
 }
